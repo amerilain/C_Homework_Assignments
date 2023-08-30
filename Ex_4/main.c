0dbf5242a0dab2b01404b06f6df7db0a25a14195
amerilain <amerilain@gmail.com>
2023-08-30 16:43:59 +0300

    Exercise 4 finished

--- Ex_4/main.c
+++ Ex_4/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+// Function declaration
+int read_integer(void);
+
 int main() {
-    printf("Hello, World!\n");
+    int num, count = 0;
+    double sum = 0.0;
+    int continueReading = 1; // Control variable for the loop
+
+    // Loop to read integers until a negative number is entered
+    while (continueReading) {
+        printf("Enter positive numbers or negative to stop: ");
+        num = read_integer(); // Call read_integer function to read an integer
+
+        if (num < 0) {
+            continueReading = 0; // Set control variable to false if a negative number is entered
+        } else {
+            sum += num; // Add the entered number to the sum
+            count++; // Increment the count of entered numbers
+        }
+    }
+
+    // Calculate and print the average
+    if (count > 0) {
+        double average = sum / count;
+        printf("You entered %d positive numbers. The average is: %.3f\n", count, average);
+    } else {
+        printf("No positive numbers entered.\n");
+    }
+
     return 0;
+}
+
+// Function definition
+int read_integer(void) {
+    int num;
+    int isValid = 0;
+
+    while (!isValid) {
+        if (scanf("%d", &num) == 1) {
+            isValid = 1;
+        } else {
+            // Clear the input buffer and print an error message
+            while (getchar() != '\n');
+            printf("invalid input\n");
+        }
+    }
+    return num;
 }
